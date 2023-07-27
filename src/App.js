73cd65c529736208b9eb59a1f71f75385171b37f
LAPTOP-TL6DVKOG\munkylo <donam2832000@gmail.com>
2023-07-27 11:53:54 +0700

    test conflict

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ function App() {
   return (
     <div className="App">
 
-
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -19,6 +18,7 @@ function App() {
         >
           Learn React
         </a>
+        <div>test</div>
       </header>
     </div>
   );
